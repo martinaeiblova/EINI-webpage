a5c6a7089aa0474c8718352ab7c554adc0b05ec6
Martina Eiblová <emartina@seznam.cz>
2023-07-20 15:00:49 +0200

    Button - scrolling window

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 "use strict";
 
 const header = document.querySelector("header");
+const section1 = document.querySelector("#section--1");
+const btnScroll = document.querySelector(".btn-scroll");
+
 const cookieMessage = document.createElement("div");
 cookieMessage.classList.add("cookie-message");
 cookieMessage.innerHTML =
@@ -10,3 +13,9 @@ header.after(cookieMessage);
 document
     .querySelector(".cookie-message")
     .addEventListener("click", cookieMessage.remove);
+
+//Scrolling
+btnScroll.addEventListener("click", function () {
+    console.log(section1.getBoundingClientRect());
+    section1.scrollIntoView({ behavior: "smooth" });
+});
