25e7d57bb1ceaae1808f51090a636c3c484e6b51
Martina Eiblová <emartina@seznam.cz>
2023-07-20 20:17:25 +0200

    Scrolling to sections after clicking nav-item

--- script.js
+++ script.js
@@ -19,3 +19,14 @@ btnScroll.addEventListener("click", function () {
     console.log(section1.getBoundingClientRect());
     section1.scrollIntoView({ behavior: "smooth" });
 });
+
+document.querySelector(".nav-link").addEventListener("click", function (e) {
+    e.preventDefault();
+
+    if (e.target.classList.contains("nav--item")) {
+        const id = e.target.getAttribute("id");
+        document.querySelector(id).scrollIntoView({
+            bahavior: "smooth",
+        });
+    }
+});
